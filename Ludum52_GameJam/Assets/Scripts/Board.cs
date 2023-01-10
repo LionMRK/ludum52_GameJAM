e554d96b4ff67a9eb110d10660c7f9c3d0b56469
Rodrigo <rodrigo_m_rodrigues@hotmail.com>
2023-01-09 22:14:03 -0300

    Early board/card codes'
    go
    go
    go
    go

--- Ludum52_GameJam/Assets/Scripts/Board.cs
+++ Ludum52_GameJam/Assets/Scripts/Board.cs
@@ -1,28 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Board : MonoBehaviour 
 {
     public static Board board;
 
-    public Cards[,] map;
+    public GameObject[,] map;
+    public GameObject[,] ground;
 
     private void Awake()
     {
         board = this;
-        map = new Cards[4, 4];
+        map = new GameObject[4, 4];
+        ground = new GameObject[4, 4];
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int x = 0; x < 4; x++)
+        {
+            for(int y = 0; y < 4; y++)
+            {
+                ground[x, y] = board.transform.GetChild(x+y).gameObject;
+            }
+        }    
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public bool CanPlace(GameObject location)
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                if (location == ground[x,y])
+                {
+                    if (map[x, y] == null)
+                    {
+                        return true;
+                    }
+
+                }
+            }
+        }
+        return false;
+    }
+
+    public void AddCard(GameObject card, GameObject location)
+    {
+
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                if(location == ground[x,y])
+                {
+                    map[x, y] = card;
+                }
+
+            }
+        }
     }
 }
--- Ludum52_GameJam/Assets/Scripts/Player.cs
+++ Ludum52_GameJam/Assets/Scripts/Player.cs
@@ -1,30 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Android;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class Player : MonoBehaviour
 {
-
-    public CardObject[] hand;
+    public List<GameObject> hand;
     public Transform[] hand_positions;
+    public GameObject prefabCard;
 
     public GameObject holded_card;
     public bool is_holdind;
 
     public RaycastHit2D raycast;
+
+    public Vector2 oldPos;
     // Start is called before the first frame update
     void Start()
     {
         is_holdind = false;
 
-        hand = new CardObject[6];
+        for(int i = 0; i < 6; i++)
+        {
+            hand.Add(GameObject.Instantiate(prefabCard));
+            hand[i].transform.position = hand_positions[i].position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        gameObject.transform.position = new Vector3(mouse.x, mouse.y, 0);
 
         raycast = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector3.forward);
 
@@ -38,6 +47,27 @@ public class Player : MonoBehaviour
                 }
 
             }
+            else if (raycast.collider.gameObject.CompareTag("Ground"))
+            {
+                Debug.Log("raio no chao");
+                if (is_holdind)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        GameObject ground;
+                        ground = raycast.collider.gameObject;
+                        if (ground.transform.parent.GetComponent<Board>().CanPlace(ground))
+                        {
+                            ground.transform.parent.GetComponent<Board>().AddCard(holded_card, ground);
+                            holded_card.gameObject.transform.position = ground.transform.position;
+                            holded_card = null;
+                            is_holdind = false;
+                            hand.Remove(holded_card);
+                        }
+
+                    }
+                }
+            }
         }
 
 
@@ -47,6 +77,15 @@ public class Player : MonoBehaviour
             pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             pos.z = 0;
             holded_card.transform.position = pos;
+            holded_card.GetComponent<BoxCollider2D>().enabled = false;
+
+            if(Input.GetMouseButton(1))
+            {
+                holded_card.GetComponent<BoxCollider2D>().enabled = true;
+                holded_card.transform.position = oldPos;
+                is_holdind = false;
+                holded_card = null;
+            }
         }
     }
 
@@ -54,7 +93,21 @@ public class Player : MonoBehaviour
     {
         //foi clicado
 
-        holded_card = card;
-        is_holdind = true;
+
+        if(!is_holdind)
+        {
+            oldPos = card.transform.position;
+            holded_card = card;
+            is_holdind = true;
+        }
+
+        //if(hand.Contains(card))
+        //{
+        //    oldPos = hand.transform.position;
+        //}
+        //oldPos = hand.(card).transform.position
+
+
+        
     }
 }
